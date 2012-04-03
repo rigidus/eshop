cd0e19570ba032fc629dc27e3f5d4d636a4c9ea2
Rigidus <avenger-f@yandex.ru>
2012-04-03 04:01:11 +0400

    product options proof of concept

--- storage/product.lisp
+++ storage/product.lisp
@@ -25,14 +25,48 @@ alter user <dbuser> with password '<dbpassword>';
 
 (connect-toplevel "restodb" "resto" "resto1111" "localhost")
 
+;; produce incrementor closure
+(defmacro incrementor (name)
+  `(let ((,(intern (format nil "INC-~A-ID" (symbol-name name))) 0))
+     (list
+      (defun ,(intern (format nil "INCF-~A-ID" (symbol-name name) ())) ()
+        (incf ,(intern (format nil "INC-~A-ID" (symbol-name name)))))
+      (defun ,(intern (format nil "INIT-~A-ID" (symbol-name name) ())) (init-value)
+        (setf ,(intern (format nil "INC-~A-ID" (symbol-name name))) init-value)))))
 
-(let ((inc-product-id 0))
-  (defun inc-product-id ()
-    (incf inc-product-id)))
+
+;; incrementor test
+(print (macroexpand-1 '(incrementor product)))
+;; (LET ((INC-PRODUCT-ID))
+;;   (LIST
+;;     (DEFUN INCF-PRODUCT-ID ()
+;;       (INCF INC-PRODUCT-ID))
+;;     (DEFUN INCF-PRODUCT-ID (INIT-VALUE)
+;;       (SETF INC-PRODUCT-ID INIT-VALUE))))
 
 
+
+;; === OPTION ===
+
+(defclass option ()
+  ((product-id        :col-type integer         :initarg :product-id     :initform 0          :accessor product-id)
+   (name              :col-type string          :initarg :name           :initform ""         :accessor name)
+   (value             :col-type string          :initarg :value          :initform ""         :accessor value)
+   (optgrp            :col-type string          :initarg :name           :initform ""         :accessor optgrp)
+   (optype            :col-type string          :initarg :name           :initform ""         :accessor opttype))
+  (:metaclass dao-class)
+  (:keys product-id name value))
+
+(execute (dao-table-definition 'option))
+
+
+;; === PRODUCT ===
+
+(incrementor product)
+
+;; class product
 (defclass product ()
-  ((id                :col-type integer         :initarg :id              :initform 0         :accessor id)
+  ((id                :col-type integer         :initarg :id              :initform (incf-product-id)  :accessor id)
    (name              :col-type string          :initarg :name            :initform ""        :accessor name)
    (price             :col-type integer         :initarg :price           :initform ""        :accessor price)
    (opts                                        :initarg :opts            :initform ""        :accessor opts))
@@ -42,137 +76,114 @@ alter user <dbuser> with password '<dbpassword>';
 ;; (execute (dao-table-definition 'product))
 
 
+;; === TESTS ===
+
+;; insert product
 (insert-dao
- (make-instance
-  'product
-  :id (inc-product-id)
+ (make-instance 'product
   :name "Тестовый Продукт"
-  :price 200
-  :opts nil))
+  :price (random 700)))
 
 (query (:select '* :from 'product))
+;; (query (:delete-from 'product))
 
-
-(let ((test-product (get-dao 'product 4)))
+;; update product
+(let ((test-product (get-dao 'product 1)))
   (setf (price test-product) 4500000)
   (update-dao test-product))
 
-(query (:select '* :from 'country_table))
-
-(dao-keys 'product)
-
-(dao-keys (get-dao 'product 4))
-
-(sql (:select 'relname :from 'pg-catalog.pg-class
-              :inner-join 'pg-catalog.pg-namespace :on (:= 'relnamespace 'pg-namespace.oid)
-              :where (:and (:= 'relkind "r")
-                           (:not-in 'nspname (:set "pg_catalog" "pg_toast"))
-                           (:pg-catalog.pg-table-is-visible 'pg-class.oid))))
 
 ;; Prepared Statements
-(defprepared sovereign-of
-    (:select 'sovereign :from 'country_table :where (:= 'name '$1))
+(defprepared price-of-product
+    (:select 'price :from 'product :where (:= 'name '$1))
   :single!)
 
-(sovereign-of "The Netherlands")
+(price-of-product "Тестовый Продукт")
+
+
+;; === HASH-TABLES ===
+;; Это пример, который иллюстрирует что будет, если мы будем кешировать в хэш-таблицах продукты
+;; Он довольно синтетический, но дает понимание зачем тут использовать макроc create-product
+
+(defparameter *h-product* (make-hash-table))
+
+(defmacro create-product (&body params)
+  `(let ((id (incf-product-id)))
+     (values
+      (setf (gethash id *h-product*)
+            (make-dao
+             'product
+             :id id
+             ,@params))
+      id)))
+
+(print (macroexpand-1 '(create-product :name "Еще продукт" :price (random 700))))
+;; (LET ((ID (INCF-PRODUCT-ID)))
+;;   (VALUES
+;;    (SETF (GETHASH ID *H-PRODUCT*)
+;;          (MAKE-DAO 'PRODUCT :ID ID :NAME "Еще продукт" :PRICE (RANDOM 700)))
+;;    ID))
+
+(create-product :name "Еще продукт" :price (random 700))
+
+
+(make-dao 'option
+          :product-id 3
+          :name "опт-name2"
+          :value "опт-value2")
+
+(query (:select '* :from 'option))
+
+
+(add-option-to-product-by-id 5 "оптнаме2" "оптвалуе2")
 
 
 
-(defun add-product (name)
-  "Проверка на дубликат в БД"
-  (make-dao
-   'product
-   :price (inc-product-id)
-   :name "wefew"
-   :id 6))
+;; get options
+(query (sql (:select '* :from 'product
+                     :inner-join 'option :on (:= 'product.id 'option.product-id)
+                     :where (:= 'product.id 3))))
 
 
-
-(defparameter *opname-id* 0
-    "Переменная автоинкремента для opname")
-
-(defun *opname-id* ()
-    "Автоинкремент для opname"
-      (incf *opname-id*))
-
-(defclass opname ()
-    ((id :col-type integer :initarg :id :accessor opnamet-id)
-        (name :col-type string :initarg :name :accessor opname-name))
-    (:metaclass dao-class)
-      (:keys id))
-
-(defun add-opname (name)
-    "Проверка на дубликат в БД"
-    (make-dao 'opname
-              :id (*opname-id*)
-              :name name))
-
-(defparameter *option-id* 0
-  "Переменная автоинкремента для option")
-
-(defun *option-id* ()
-  "Автоинкремент для option"
-  (incf *option-id*))
-
-(defclass option ()
-  ((pr-id :col-type integer :initarg :pr-id :accessor option-pr-id)
-   (op-id :col-type integer :initarg :op-id :accessor option-op-id)
-   (value :col-type string :initarg :value :accessor option-value))
-  (:metaclass dao-class)
-  (:keys pr-id op-id value))
-
-(defun add-option (pr-name &rest opt-list)
-  "opt-list должен быть plist-ом"
-  (unless (and opt-list
-               (evenp (length opt-list)))
-    (format t "неправильный opt-list")
-    (return-from add-option))
-  (let ((prod-list (car (select-dao 'product (:ilike 'name pr-name)))))
-    (when (null prod-list)
-      (setf prod-list (add-product pr-name)))
-    (loop :for i :from 0 :upto (1- (length opt-list)) :by 2
-       :do (let* ((op-key (nth i opt-list))
-                  (op-val (nth (1+ i) opt-list))
-                  (key-list (car (select-dao 'opname (:ilike 'name op-key)))))
-             (when (null key-list)
-               (setf key-list (add-opname op-key)))
-             (make-dao 'option
-                       :pr-id (product-id prod-list)
-                       :op-id (opnamet-id key-list)
-                       :value op-val)))))
+;; (defun add-option (pr-name &rest opt-list)
+;;   "opt-list должен быть plist-ом"
+;;   (unless (and opt-list
+;;                (evenp (length opt-list)))
+;;     (format t "неправильный opt-list")
+;;     (return-from add-option))
+;;   (let ((prod-list (car (select-dao 'product (:ilike 'name pr-name)))))
+;;     (when (null prod-list)
+;;       (setf prod-list (add-product pr-name)))
+;;     (loop :for i :from 0 :upto (1- (length opt-list)) :by 2
+;;        :do (let* ((op-key (nth i opt-list))
+;;                   (op-val (nth (1+ i) opt-list))
+;;                   (key-list (car (select-dao 'opname (:ilike 'name op-key)))))
+;;              (when (null key-list)
+;;                (setf key-list (add-opname op-key)))
+;;              (make-dao 'option
+;;                        :pr-id (product-id prod-list)
+;;                        :op-id (opnamet-id key-list)
+;;                        :value op-val)))))
 
 
-(execute (dao-table-definition 'product))
+;; (execute (dao-table-definition 'product))
+;; (execute (dao-table-definition 'opname))
+;; (execute (dao-table-definition 'option))
+;; (query "ALTER TABLE product ADD CONSTRAINT uq_p_name UNIQUE (name)")
+;; (query "ALTER TABLE product ADD CHECK (name <> '')")
+;; (query "ALTER TABLE opname ADD CONSTRAINT uq_o_name UNIQUE (name)")
+;; (query "ALTER TABLE opname ADD CHECK (name <> '')")
+;; (query "ALTER TABLE option ADD FOREIGN KEY (pr_id) REFERENCES product(id)")
+;; (query "ALTER TABLE option ADD FOREIGN KEY (op_id) REFERENCES opname(id)")
+;; (add-product "oil")
 
-(execute (dao-table-definition 'opname))
+;; (add-product "масло A")
+;; (add-product "масло B")
+;; (add-opname "объём")
+;; (add-opname "вязкость")
 
-(execute (dao-table-definition 'option))
-
-(query "ALTER TABLE product ADD CONSTRAINT uq_p_name UNIQUE (name)")
-
-(query "ALTER TABLE product ADD CHECK (name <> '')")
-
-(query "ALTER TABLE opname ADD CONSTRAINT uq_o_name UNIQUE (name)")
-
-(query "ALTER TABLE opname ADD CHECK (name <> '')")
-
-(query "ALTER TABLE option ADD FOREIGN KEY (pr_id) REFERENCES product(id)")
-
-(query "ALTER TABLE option ADD FOREIGN KEY (op_id) REFERENCES opname(id)")
-
-(add-product "oil")
-
-(add-product "масло A")
-(add-product "масло B")
-(add-opname "объём")
-(add-opname "вязкость")
-
-(add-option "oil" "объём" "3л." "объём" "4л.")
-
-(add-option "oil" "объём" "3,5л." "объём" "4,5л.")
-
-(add-option "масло U" "вес" "7,5г")
-
-(add-option "масло U" "вес" "7,5г" "gjh")
-
-(add-option "oil" "объём" "3,5л." "объём" "4,5л.")
+;; (add-option "oil" "объём" "3л." "объём" "4л.")
+;; (add-option "oil" "объём" "3,5л." "объём" "4,5л.")
+;; (add-option "масло U" "вес" "7,5г")
+;; (add-option "масло U" "вес" "7,5г" "gjh")
+;; (add-option "oil" "объём" "3,5л." "объём" "4,5л.")
