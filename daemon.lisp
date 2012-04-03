2d00531611d3880c5db0c9301c7e60555c62a96a
Rigidus <avenger-f@yandex.ru>
2012-04-03 04:08:30 +0400

    show started

--- daemon.lisp
+++
@@ -1,4 +0,0 @@
-(ql:quickload 'swank)
-(swank:create-server :dont-close t :port 6777)
-(push (merge-pathnames "repo/eshop/" (user-homedir-pathname))  asdf:*central-registry*)
-(asdf:oos 'asdf:load-op :eshop)
